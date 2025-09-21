152e55471b68add920458b7a88400bd7d49b9574
Ayush Shrivastava <2024.ayush.shrivastava@ves.ac.in>
2025-09-21 13:17:51 +0530

    card n btn color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Card = (props) => {
   return (
     <div>
-        <div className=' mr-5 bg-black text-white inline-block text-center rounded-md p-6'>
+        <div className=' mr-5 bg-white text-black inline-block text-center rounded-md p-6'>
           <img className='ml-2 h-32 w-32 rounded-full mb-3' src="{props.profile}" alt="" />
             <h1 className='text-2xl font-semibold mb-4'>{props.name}</h1>
             <h2 className='m-4'>{props.age}, {props.gender}, {props.city}</h2>
-            <button className='mt-5 bg-emerald-600 px-2 py-1 rounded-md hover:bg-white hover:text-emerald-600'>Add Friend</button>
+            <button className='mt-5 bg-emerald-600 text-white px-2 py-1 border-emerald-600 border-2 rounded-md hover:bg-white hover:text-emerald-600'>Add Friend</button>
         </div>
     </div>
   )
